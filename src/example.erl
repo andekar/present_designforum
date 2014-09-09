5b00d61bb8ba0801883785eb6d4925272fd25c1f
Anders Karlsson <andersk84@gmail.com>
2014-09-09 13:46:54 +0200

    last try

--- src/example.erl
+++ src/example.erl
@@ -7,8 +7,8 @@ delete_element_try1(_, []) ->
     [];
 delete_element_try1(Element, [Element|List]) ->
     List;
-delete_element_try1(Element, [_Other|List]) ->
-    delete_element_try1(Element, List).
+delete_element_try1(Element, [Other|List]) ->
+    [Other|delete_element_try1(Element, List)].
 
 prop_delete_0_try1() ->
     ?FORALL({X, Xs}, {int(), list(int())},
